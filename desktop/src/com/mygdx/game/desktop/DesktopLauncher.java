5f14d8c6e9c57de6b891e1d4ae218592dd278810
manalmohania <u6019071@anu.edu.au>
2017-06-14 21:02:49 +1000

    Changed default window size

--- desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -8,8 +8,8 @@ public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		config.title = "Token Soccer";
-		config.height = 400;
-		config.width = 500;
+		config.height = 480;
+		config.width = 640;
 		new LwjglApplication(new TokenSoccer(), config);
 	}
 }
